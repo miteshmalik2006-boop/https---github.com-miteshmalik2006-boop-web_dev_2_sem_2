045889112e4e39ce235a445065eaf4e79e73e3c7
unknown <you@example.com>
2026-02-25 20:23:29 +0530

    initial commit

---
+++ web_dev12/script.js
@@ -0,0 +1,13 @@
+
+let pro = new Promise((resolve, reject) => {
+    let proposal = "false"
+    if (proposal == "true") {
+        resolve()
+    }
+    else {
+        reject()
+    }
+    pro.then(() => console.log("proposal accepted"))
+        .catch(() => console.log("proposal rejected"))
+
+})
